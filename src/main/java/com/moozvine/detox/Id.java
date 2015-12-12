4073aca1d54f6a4f5c704973b213aecbd96944a4
Rich Martin <rs.martin@computer.org>
2015-12-12 12:59:13 +0100

    Fix the javadoc so it doesn't have an accidental doc tag.

--- src/main/java/com/moozvine/detox/Id.java
+++ src/main/java/com/moozvine/detox/Id.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Target;
 /**
  * Declares that the tagged member forms part of the identity of the object.
  * Two instances are considered equal if they are the same instance or if all
- * @Id members are equal.
+ * id-tagged members are equal.
  */
 @Target(ElementType.METHOD)
 public @interface Id {
